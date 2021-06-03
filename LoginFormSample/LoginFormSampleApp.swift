07a53f0abdac3612548f304c16ed9f66ae0820b7
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-04 08:48:55 +0900

    Initial Commit

---
+++ LoginFormSample/LoginFormSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormSampleApp.swift
+//  LoginFormSample
+//
+//  Created by Yusuke Hasegawa on 2021/06/04.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
